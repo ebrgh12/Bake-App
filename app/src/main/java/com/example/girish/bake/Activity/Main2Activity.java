ba201ac3e75aa3f32df6747f520c3560834e426e
Girish <girish12ramesh@gmail.com>
2018-01-27 23:47:34 -0800

    Tried for next screen

--- app/src/main/java/com/example/girish/bake/Activity/Main2Activity.java
+++ app/src/main/java/com/example/girish/bake/Activity/Main2Activity.java
@@ -3,10 +3,15 @@ package com.example.girish.bake.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.girish.bake.Model.BakeResponse;
 import com.example.girish.bake.R;
 
-public class Main2Activity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main2Activity extends AppCompatActivity {
+   // List<BakeResponse> mListDataModels = new ArrayList<BakeResponse>();
+    MainActivity mListDataModels;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
